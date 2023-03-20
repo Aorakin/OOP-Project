185287a5e194ce6fda321459d97f1dafa47a99a1
Aorakin <>
2023-03-21 00:53:26 +0700

    separate class

---
+++ Facility.py
@@ -0,0 +1,62 @@
+class Facility():
+    def __init__(self,pets,ev_charger,salon,laudry,store,restaurant,security,cctv,finger_print,keycard,fitness,pool,lift,parking,smoking):
+        self.__pets = pets
+        self.__ev_charger = ev_charger
+        self.__salon = salon
+        self.__laudry = laudry
+        self.__store = store
+        self.__restaurant = restaurant
+        self.__security = security
+        self.__cctv = cctv
+        self.__finger_print = finger_print
+        self.__keycard = keycard
+        self.__fitness = fitness
+        self.__pool = pool
+        self.__lift = lift
+        self.__parking = parking
+        self.__smoking = smoking
+
+    def get__pets(self):
+        return self.__pets
+    
+    def get__ev_charger(self):
+        return self.__ev_charger
+    
+    def get__salon(self):
+        return self.__salon
+    
+    def get__laudry (self):
+        return self.__laudry
+    
+    def get__store(self):
+        return self.__store
+    
+    def get__restaurant (self):
+        return self.__restaurant
+    
+    def get__security (self):
+        return self.__security
+    
+    def get__cctv (self):
+        return self.__cctv
+    
+    def get__finger_print(self):
+        return self.__finger_print
+    
+    def get__keycard (self):
+        return self.__keycard
+    
+    def get__fitness (self):
+        return self.__fitness
+    
+    def get__pool (self):
+        return self.__pool
+    
+    def get__lift (self):
+        return self.__lift
+    
+    def get__parking(self):
+        return self.__parking
+    
+    def get__smoking(self):
+        return self.__smoking
