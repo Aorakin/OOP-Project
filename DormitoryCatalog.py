01fcba7058db48a6356847e8327b6282d8ab92ad
Vorachot Banluepongpan <armz2928@gmail.com>
2023-03-21 01:16:15 +0700

    first commit

---
+++ DormitoryCatalog.py
@@ -0,0 +1,16 @@
+class DormitoryCatalog:
+    def __init__(self):
+        self._dormitory = []
+        self._boost_dormity = []
+    def check_facility():
+        pass
+    def save_to_dormitory_list():
+        pass
+    def check_price(self,max_price):
+        pass
+    def create_dormitory(self,name,address,detail,phone,electric,warter,sevice_fee,internet,dorm_picture,term_of_service):
+        pass
+    def get_dormitory_list(self,name_dor):
+        pass
+    def get_dormitory_list(self):
+        pass
