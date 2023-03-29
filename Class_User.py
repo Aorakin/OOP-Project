2b798e9b6b47ca338312a7326954e4c8bada9cb6
Mxrnlnguoou <106077189+Morninguoou@users.noreply.github.com>
2023-03-29 13:41:13 +0700

    push to accountList

--- Class_User.py
+++ Class_User.py
@@ -1,3 +1,5 @@
+from AccountList import AccountList
+
 class User:
     def __init__(self,name,lastname,email,user_name,password,user_phone):
         self._name = name
@@ -6,5 +8,17 @@ class User:
         self._user_name = user_name
         self._password = password
         self._user_phone = user_phone
-    def add_user_toList(User):
-        pass
+        self.users = {}
+
+    def register(self):
+        name = input("Enter name: ") #เอาไว้แก้เป็นดึงข้อมูลจากหน้าบ้าน
+        lastname = input("Enter lastname")
+        email = input("Enter email: ")
+        user_name = input("Enter UserName")
+        password = input("Enter password: ")
+        user_phone = input("Enter phone number")
+
+        # Add a new user to the accountlist
+        account_list = AccountList()
+        new_user = User(name,lastname,email,user_name,password,user_phone)
+        account_list.add_account(new_user)
