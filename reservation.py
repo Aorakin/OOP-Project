b7a6760b2e9c0ccf7eaf89aa331583de138936ff
blwchn <65010973@kmitl.ac.th>
2023-03-29 16:47:24 +0700

    search_dor_name

--- reservation.py
+++ reservation.py
@@ -1,8 +1,8 @@
-class Reservation:
+class Reservation(Room):
     def __init__(self,check_in,reservation_detail,calendar):
-        self._check_in = check_in
-        self._reservation = reservation_detail
-        self._calendar = calendar
-
-    def get_detail_payment(self):
+        Room.__init__(self,room_id,room_rental,room_status,room_fac):
+        self.__check_in = check_in
+        self.__reservation_detail = reservation_detail
+        self.__calendar = calendar
+    def get_detail_payment():
         pass
